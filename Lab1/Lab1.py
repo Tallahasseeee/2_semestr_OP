3d2e9de5bf0998ad9f737b88727e619ad50da25c
Tallahasseeee <90040962+Tallahasseeee@users.noreply.github.com>
2022-02-24 03:25:29 +0200

    Delete Lab1.py

--- Lab1/Lab1.py
+++
@@ -1,8 +0,0 @@
-import math
-R = float(input("Введіть радіус основи: "))
-H = float(input("Введіть висоту: \n"))
-if R > 0 and H > 0:
-    print("Об'єм циліндра = " + str(math.pi*R*R*H))
-    print("Об'єм циліндра = " + str(math.pi*R*R*H/3))
-else:
-    print("Введено некоректні дані")
