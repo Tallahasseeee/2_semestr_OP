e97f75d5aea8837a6069e5b00f1023ee6c0b85f9
Tallahasseeee <90040962+Tallahasseeee@users.noreply.github.com>
2022-06-16 03:32:18 +0300

    Add files via upload

---
+++ Lab6/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace Lab6_second_semestr
+{
+    public class Program
+    {
+        private static int Count(char sym, string line)
+        {
+            int counter = 0;
+            foreach(char s in line)
+            {
+                if(s == sym)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+
+        private static bool Contains(int j, string line)
+        {
+            for (int i = 0; i < j; i++)
+            {
+                if(line[j] == line[i])
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        private static BinaryTree Process(string line)
+        {
+            BinaryTree tree = new BinaryTree(line[0], Count(line[0],line), null);
+            for (int i = 1; i < line.Length; i++)
+            {
+                if (!Contains(i, line))
+                {
+                    tree.Add(line[i], Count(line[i], line));
+                }
+            }
+            return tree;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введіть рядок символів");
+            string line = Console.ReadLine();
+            BinaryTree tree = Process(line);
+            tree.PrintTree(tree,0);
+        }
+    }
+}
---
+++ Lab6/Tree.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace Lab6_second_semestr
+{
+    public class BinaryTree
+    {
+        private BinaryTree parent, left = null, right = null;
+        private char sym;
+        private int val;
+
+        public BinaryTree(char sym, int val, BinaryTree parent)
+        {
+            this.sym = sym;
+            this.val = val;
+            this.parent = parent;
+        }
+
+        public void Add(char sym, int val)
+        {
+            if (val < this.val)
+            {
+                if (this.left == null)
+                {
+                    this.left = new BinaryTree(sym, val, this);
+                }
+                else if (this.left != null)
+                {
+                    this.left.Add(sym, val);
+                }
+            }
+            else
+            {
+                if (this.right == null)
+                {
+                    this.right = new BinaryTree(sym, val, this);
+                }
+                else if (this.right != null)
+                {
+                    this.right.Add(sym, val);
+                }
+            }
+        }
+
+         public void PrintTree(BinaryTree tree, int tabs)
+        {
+            if (tree == null)
+            {
+                return;
+            }
+            tree.PrintTree(tree.right, ++tabs);
+
+            for (int i = 1; i < tabs; i++)
+            {
+                Console.Write("\t");
+            }
+            Console.Write(tree.sym + "\n");
+
+            tree.PrintTree(tree.left, tabs);
+        }
+
+
+    }
+}
